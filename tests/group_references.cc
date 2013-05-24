6e7d6373fbbe51f8f9a9a0a9d371a37793f8d45b
Florian Knauf <f.knauf@mmkf.de>
2013-05-25 01:36:26 +0200

    ref in Konstruktor: Copy/Move getestet.

--- tests/group_references.cc
+++ tests/group_references.cc
@@ -13,20 +13,29 @@ namespace {
 
   int default_ref;
   struct ref_impossible : std::exception { };
+  struct ref_null       : std::exception { };
 
   class reference_holder : public reference_base {
   public:
-    reference_holder(reference_holder &&) = delete;
+    reference_holder(reference_holder const &) = default;
+    reference_holder(reference_holder &&other) : ptr_(other.ptr_) {
+      other.ptr_ = nullptr;
+    }
 
-    reference_holder(int &ref) : ref_(ref) { }
-    reference_holder(int &&  ) : ref_(default_ref) {
+    reference_holder(int &ref) : ptr_(&ref) { }
+    reference_holder(int &&  ) {
       throw ref_impossible();
     }
 
-    virtual int &val() const { return ref_; }
+    virtual int &val() const {
+      if(!ptr_) {
+	throw ref_null();
+      }
+      return *ptr_; 
+    }
 
   private:
-    int &ref_;
+    int *ptr_;
   };
 
   typedef inplace::factory<reference_base,
@@ -50,6 +59,98 @@ BOOST_AUTO_TEST_CASE(ReferenceConstruct) {
   BOOST_CHECK_THROW(fct.construct<reference_holder>(2), ref_impossible);
   BOOST_CHECK_THROW(fct.construct<reference_holder>(std::move(i)), ref_impossible);
   BOOST_CHECK(!fct);
+}
+
+BOOST_AUTO_TEST_CASE(ReferenceCopyCtor) {
+  factory_t fct;
+  int i = 10;
+
+  fct.construct<reference_holder>(i);
+  factory_t fct2(fct);
+
+  BOOST_REQUIRE(fct);
+  BOOST_REQUIRE(fct2);
+  BOOST_CHECK_EQUAL(fct2->val(), fct->val());
+  --i;
+  BOOST_CHECK_EQUAL(fct ->val(), 9);
+  BOOST_CHECK_EQUAL(fct2->val(), 9);
+}
+
+BOOST_AUTO_TEST_CASE(ReferenceMoveCtor) {
+  factory_t fct;
+  int i = 10;
+
+  fct.construct<reference_holder>(i);
+  factory_t fct2(std::move(fct));
+
+  BOOST_CHECK  (!fct);
+  BOOST_REQUIRE( fct2);
+  BOOST_CHECK_EQUAL(fct2->val(), 10);
+  --i;
+  BOOST_CHECK_EQUAL(fct2->val(),  9);
+}
+
+BOOST_AUTO_TEST_CASE(ReferenceCopyAssign) {
+  factory_t fct, fct2;
+  int i = 10, j = 20;
+
+  fct.construct<reference_holder>(i);
+  fct2 = fct;
+
+  BOOST_REQUIRE(fct );
+  BOOST_REQUIRE(fct2);
+  BOOST_CHECK_EQUAL(fct2->val(), fct->val());
+  --i;
+  BOOST_CHECK_EQUAL(fct ->val(), 9);
+  BOOST_CHECK_EQUAL(fct2->val(), 9);
+
+  fct2 = fct;
+
+  BOOST_REQUIRE(fct );
+  BOOST_REQUIRE(fct2);
+  BOOST_CHECK_EQUAL(fct2->val(), fct->val());
+  --i;
+  BOOST_CHECK_EQUAL(fct ->val(), 8);
+  BOOST_CHECK_EQUAL(fct2->val(), 8);
+
+  fct.construct<reference_holder>(j);
+  fct2 = fct;
+
+  BOOST_REQUIRE(fct );
+  BOOST_REQUIRE(fct2);
+  BOOST_CHECK_EQUAL(fct2->val(), fct->val());
+  --j;
+  BOOST_CHECK_EQUAL(fct ->val(), 19);
+  BOOST_CHECK_EQUAL(fct2->val(), 19);
+}
+
+BOOST_AUTO_TEST_CASE(ReferenceMoveAssign) {
+  factory_t fct, fct2;
+  int i = 10, j = 20;
+
+  fct.construct<reference_holder>(i);
+  fct2 = std::move(fct);
+
+  BOOST_CHECK  (!fct );
+  BOOST_REQUIRE( fct2);
+  --i;
+  BOOST_CHECK_EQUAL(fct2->val(), 9);
+
+  fct.construct<reference_holder>(i);
+  fct2 = std::move(fct);
+
+  BOOST_CHECK  (!fct );
+  BOOST_REQUIRE( fct2);
+  --i;
+  BOOST_CHECK_EQUAL(fct2->val(), 8);
+
+  fct.construct<reference_holder>(j);
+  fct2 = std::move(fct);
+
+  BOOST_CHECK  (!fct );
+  BOOST_REQUIRE( fct2);
+  --j;
+  BOOST_CHECK_EQUAL(fct2->val(), 19);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
